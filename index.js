ef156ca78de5a042e826c33870de7d765d00515d
Eric Adams <adams.ea@gmail.com>
2018-03-24 15:18:30 -0400

    List memes and images

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dynogels.AWS.config.update({
 
 // DynamoDB Models
 const Joi = require('joi');
-let Meme = dynogels.define('Meme', {
+let Memes = dynogels.define('Meme', {
     hashKey: 'id',
 
     // add the timestamp attributes (updatedAt, createdAt)
@@ -16,9 +16,11 @@ let Meme = dynogels.define('Meme', {
         id: dynogels.types.uuid(),
         title: Joi.string(),
         src: Joi.string().uri()
-    }
+    },
+
+    tableName: process.env.MEMES_TABLE
 });
-let Image = dynogels.define('Image', {
+let Images = dynogels.define('Image', {
     hashKey: 'id',
 
     // add the timestamp attributes (updatedAt, createdAt)
@@ -28,16 +30,61 @@ let Image = dynogels.define('Image', {
         id: dynogels.types.uuid(),
         title: Joi.string(),
         src: Joi.string().uri()
-    }
+    },
+
+    tableName: process.env.IMAGES_TABLE
 });
 
 // Express config
 const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
+// Enable CORS for all methods
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token");
+    res.header("Access-Control-Allow-Methods", "DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT");
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello GDI!')
+});
+
+app.get('/images', (req, res) => {
+    Images
+        .scan()
+        .exec((err, data) => {
+            if (err) {
+                return res.status(500).json({
+                    message: err.message
+                });
+            }
+            let images = data && data.Items || [];
+            return res.json({
+                images,
+                count: data && data.Count || 0
+            });
+        });
+});
+
+app.get('/memes', (req, res) => {
+    Memes
+        .scan()
+        .exec((err, data) => {
+            if (err) {
+                return res.status(500).json({
+                    message: err.message
+                });
+            }
+            let images = data && data.Items || [];
+            return res.json({
+                images,
+                count: data && data.Count || 0
+            });
+        });
 });
 
 module.exports.handler = serverless(app);
