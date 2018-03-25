15be95d9390c7ff77a94c4511ffbe5b3c5b03457
Eric Adams <adams.ea@gmail.com>
2018-03-25 17:07:48 -0400

    Get memes endpoint working

--- index.js
+++ index.js
@@ -37,7 +37,10 @@ const express = require('express');
 const app = express();
 
 // Other dependencies
+const fs = require('fs');
 const uuidv4 = require('uuid/v4');
+const memecanvas = require('memecanvas');
+memecanvas.init('/tmp', '-meme');
 
 // Enable CORS for all methods
 app.use(function (req, res, next) {
@@ -48,7 +51,9 @@ app.use(function (req, res, next) {
 });
 
 // Enable POST body support
-app.use(bodyParser.json({ strict: false }));
+app.use(bodyParser.json({
+    strict: false
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello GDI!')
@@ -125,6 +130,59 @@ app.get('/memes', (req, res) => {
                 count: data && data.Count || 0
             });
         });
+});
+
+app.post('/memes', (req, res) => {
+    let src = req.body.src;
+    let title = req.body.title;
+    let topText = req.body.top_text;
+    let bottomText = req.body.bottom_text;
+    if (src && title && topText && bottomText) {
+        return memecanvas.generate(src, topText, bottomText, (err, filePath) => {
+            if (err) {
+                return res.status(500).json({
+                    message: err.message
+                });
+            }
+            return fs.readFile(filePath, (err, buf) => {
+                if (err) {
+                    return res.status(500).json({
+                        message: err.message
+                    });
+                }
+                fs.unlinkSync(filePath);
+                let name = filePath.split('/').slice(-1)[0];
+                return S3.upload({
+                        Body: buf,
+                        Bucket: process.env.IMAGES_BUCKET,
+                        ContentType: 'image/jpeg',
+                        Key: name
+                    })
+                    .promise()
+                    .then((result) =>
+                        Memes.create({
+                            title: result.Key,
+                            src: result.Location
+                        }, (err, newMeme) => {
+                            if (err) {
+                                return res.status(500).json({
+                                    message: err.message
+                                });
+                            }
+                            return res.json(newMeme.get());
+                        })
+                    )
+                    .catch((err) =>
+                        res.status(500).json({
+                            message: err.message
+                        })
+                    )
+            });
+        })
+    }
+    res.status(500).json({
+        message: 'Cannot process meme without all data'
+    });
 });
 
 module.exports.handler = serverless(app);
